5e232f5b64063256c6f5f1f7f28dfbe81ccb7b75
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2021-01-11 02:13:17 -0500

    update todo

--- src/main/kotlin/com/randochat/main/spring_app/endpoints/UpdateAccount.kt
+++ src/main/kotlin/com/randochat/main/spring_app/endpoints/UpdateAccount.kt
@@ -1,7 +1,19 @@
 package com.randochat.main.spring_app.endpoints
 
-class UpdateAccount {
+import com.fasterxml.jackson.databind.JsonNode
+import com.randochat.main.spring_app.database.AccountRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UpdateAccount @Autowired constructor(final val accountRepo: AccountRepository){
     //todo check bio length
     //verify, image
     //name/email/password/ changes
+    @PostMapping
+    fun update(@RequestBody body: JsonNode){
+        //parse json, for intry in json uupdate.validate in another method
+    }
 }
